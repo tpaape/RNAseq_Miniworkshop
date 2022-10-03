25741df0ad3ff853625329f0783846d749425f7a
mclear73 <mclear73@gmail.com>
2022-10-03 12:53:51 -0400

    updated covertTMM function

--- R Script/convertTMM.R
+++ R Script/convertTMM.R
@@ -2,12 +2,26 @@ rawCounts_toTMM <- function(counts, metadata, treatment_Column) {
   library(edgeR)
   library(stringr)
   #Import count data. Assumes the first column contains the gene identifier
-  countData <- read.csv(counts, row.names=1, sep='\t')
+  if (grepl('.csv', counts, fixed=TRUE) == TRUE) {
+  countData <- read.csv(counts, row.names=1)
+  } else if (grepl('.tsv', counts, fixed=TRUE) == TRUE) {
+    countData <- read.csv(counts, row.names=1, sep='\t')
+  } else if (grepl('.txt.', counts, fixed=TRUE) == TRUE) {
+    print('.txt file provided as counts input. Assumming it is tab delimited.')
+    countData <- read.csv(counts, row.names=1, sep='\t')
+  }
   
   #Import metadata. Assumes the first column contains the sample identifier
-  metaData <- read.csv(metadata, sep='\t')
+  if (grepl('.csv', metadata, fixed=TRUE) == TRUE) {
+    metaData <- read.csv(metadata)
+  } else if (grepl('.tsv', metadata, fixed=TRUE) == TRUE) {
+    metaData <- read.csv(metadata, sep='\t')
+  } else if (grepl('.txt.', metadata, fixed=TRUE) == TRUE) {
+    print('.txt file provided as metadat input. Assumming it is tab delimited.')
+    metaData <- read.csv(metadata, sep='\t')
+  }
   
-  metaData$Name <- paste('X', metaData$Name, sep='')
+  #metaData$Name <- paste('X', metaData$Name, sep='') #an X may be added to sample names by default if they start with a number. Uncomment this line if you need an X added to metaData sample names to match count file. 
   
   for_groups <- data.frame(colnames(countData))
   metaData$Name <- str_replace(metaData$Name, '-', '.')
@@ -15,7 +29,6 @@ rawCounts_toTMM <- function(counts, metadata, treatment_Column) {
   
   col <- grep(treatment_Column, colnames(for_groups))
 
-  
   for_groups <- for_groups[match(colnames(countData), for_groups$colnames.countData.),]
 
   groups <- factor(for_groups[[col]])
@@ -44,6 +57,3 @@ rawCounts_toTMM(counts='13320077-counts.txt',
                 metadata='test_metadata.txt', 
                 treatment_Column="Treatment")
 
-rawCounts_toTMM(counts="C:/Users/mclea/OneDrive/Brookhaven/RNAseq/Time_Series/Poplar/Raw Output/20400Tim-raw_genes_counts_INT_both_outliers_removed.csv", 
-                metadata="C:/Users/mclea/OneDrive/Brookhaven/RNAseq/Time_Series/All_metadata_pop_noOutliers.csv", 
-                treatment_Column="TissueXTrtXTime")
